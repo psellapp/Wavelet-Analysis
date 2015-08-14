d1fe64f682fcdc6b2a04c2c653165dd31dd4b334
Prabu Sellappan <psellapp@users.noreply.github.com>
2015-08-13 21:51:54 -0700

    Create wavenumber array
    Create the wavenumber (or Fourier frequencey) array for wavelet
    analysis.

---
+++ create_wave_array.m
@@ -0,0 +1,13 @@
+function [waveArray]=create_wave_array(n,dt)
+% 
+% Create the wavenumber (or Fourier frequencey) array for wavelet analysis. 
+% The first N/2 frequencies are n=0 -> n/2; the remainder are from 
+% -n/2+1 -> -1, in that order - there are a total N independent fequencies.
+% 
+% created: Prabu, 8/12/2015. Based on GRS' version of wv1d_e.f 
+% 
+
+waveArray = 1:fix(n/2);
+% k = k.*(2*pi()/(n*dt));
+waveArray = [0, waveArray, -waveArray(fix((n-1)/2):-1:1)];
+end
