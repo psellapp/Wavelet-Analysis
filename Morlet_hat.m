cea9a321f26504a6c7ade943e3b74fcc78dda057
Prabu Sellappan <psellapp@users.noreply.github.com>
2015-08-13 21:52:36 -0700

    Morlet wavelet
    Generate Morlet wavelet in frequency domain

---
+++ Morlet_hat.m
@@ -0,0 +1,21 @@
+function [Morlet_hat_coeff]=Morlet_hat(waveArray,scale,w0)
+%
+% Calculate coefficients for the analytic transform of Morlet wavelet.
+% The results are returned as complex numbers even for real-valued
+% functions.
+%
+% created: Prabu, 8/12/2015. Based on GRS' version of Morlet_hat
+%
+% waveArray - vector of wavenumbers. Generate using function create_wave_array
+% w0 - morlet hat parameter. Defaults to 5.5 if empty
+
+
+if isempty(w0)
+    w0 = 5.5;
+end
+
+arg = exp(-((scale.*waveArray)-w0).^2/2);
+Morlet_hat_coeff = complex(arg);
+plot(abs(Morlet_hat_coeff));
+
+end
