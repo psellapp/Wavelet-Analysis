da2890bc6077e5a387310adbe687eb190ad42022
Prabu Sellappan <psellapp@users.noreply.github.com>
2015-08-17 21:24:08 -0700

    Generate array of scales for wavelet analysis
    -Included option to specify scale limits

--- waveletscale.m
+++ waveletscale.m
@@ -1,10 +1,12 @@
-function [scale] = waveletscale(n,linlog_flag)
+function [scale] = waveletscale(n,linlog_flag,a0,a1)
 %
 % Generate array of scales for wavelet analysis.
 % Created: Prabu, 8/13/2015
+% modified: Prabu, 8/17/2015. Included option to specify scale limits
 %
 % n - length of original signal
 % linlog_flag - set to 0 for linear scale, 1 for log scale
+% a0,a1 - min,max values of scale a - defaults to 1 and n/2
 %
 if ~rem(n,2)==0
     disp('Error! n is not even.')
@@ -12,7 +14,17 @@ if ~rem(n,2)==0
     return
 end
 
-a0 = 1; na = n/2; a1 = na; scale = zeros(1,na);
+na = n/2;
+
+if isempty(a0)
+    a0 = 1;
+end
+if isempty(a1)
+    a1 = na;
+end
+na = a1-a0+1;
+scale = zeros(1,na);
+
 if linlog_flag %log scale
     %     disp('log scale')
     da = (a1/a0)^(1/(na-1));
