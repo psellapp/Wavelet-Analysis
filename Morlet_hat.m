9e4caacbcc924374c5ae6dec547dd5a4abcef67b
Prabu Sellappan <psellapp@usc.edu>
2016-07-19 21:47:01 -0700

    Normalized wavelet basis

--- Morlet_hat.m
+++ Morlet_hat.m
@@ -4,19 +4,23 @@ function [Morlet_hat_coeff]=Morlet_hat(waveArray,scale,w0)
 % The results are returned as complex numbers even for real-valued
 % functions.
 %
-% created: Prabu, 8/12/2015. Based on GRS' version of Morlet_hat
+% created: Prabu, 8/12/2015. 
+% Based on GRS' version of Morlet_hat and wave_bases code from Christopher
+% Torrence and Gilbert P. Compo, University of Colorado
 %
 % waveArray - vector of wavenumbers. Generate using function create_wave_array
 % w0 - morlet hat parameter. Defaults to 5.5 if empty
-
+% 
 
 if isempty(w0)
     w0 = 5.5;
 end
 
-arg = exp(-((scale.*waveArray)-w0).^2/2);
-Morlet_hat_coeff = complex(arg);
+norm = sqrt(scale*waveArray(2)*length(waveArray))/pi^(0.25); %normalize wavelet so that sum(Morlet_hat_coeff.^2)/length(waveArray) = 1
+arg = norm*exp(-(((scale.*waveArray)-w0).^2/2).*(waveArray > 0)); 
+Morlet_hat_coeff = complex(arg.*(waveArray > 0)); 
+
 %   figure(3);
-%   plot(real(ifft(Morlet_hat_coeff))) %).*(waveArray>0))+real(ifft(Morlet_hat_coeff)).*(waveArray<=0));
+%   plot(real(ifft(Morlet_hat_coeff)).*(waveArray>0)+real(ifft(Morlet_hat_coeff)).*(waveArray<=0));
 % 
 end
