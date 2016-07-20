706224421d95015fedaad78999525d35edee9470
Prabu Sellappan <psellapp@usc.edu>
2016-07-19 21:47:45 -0700

    Main function to perform wavelet analysis

--- wavelet_run_script.m
+++ wavelet_run_script.m
@@ -1,35 +1,51 @@
-function [transformSignal_array] = wavelet_run_script(test_signal,x_var,min_scale,max_scale,ds)
-% Function to perform wavelet analysis
+function [transformSignal_array,H1,H2] = wavelet_run_script(test_signal,x_var,min_scale,max_scale,ds)
+% 
+% 1-D Wavelet Analysis
 %
-% Created: Prabu, 8/13/2015
-% modified: Prabu, 8/24/2015. -generate test signal
-%           Prabu, 9/3/2015. -calculate energy
-%           Prabu, 4/20/2016. -Major changes, code actually works now! Call as a function, outputs in terms of scales themselves  
+% Created: Prabu, 8/13/2015. 
+% 
+% modified: Prabu, 8/24/2015. - generate test signal
+%           Prabu, 9/3/2015.  - calculate energy
+%           Prabu, 4/20/2016. - Major changes, code actually works now! Call as a function, outputs in terms of scales themselves
+%           Prabu, 7/19/2016. - Properly scaled wavelet to have unit energy
+%                             - Plot transformed signal as function of Morlet_scale instead of just scale to get correct values
+%
+% INPUTS: test_signal - signal to analyze
+%         x_var - independent variable, such as time or spatial coordinates
+%         min_scale, max_Scale - min and max scales of wavelet (Note: if logarithmic scales are chosen, the largest scale will be smaller than max_scale
+%         ds - scale increment, determines number of scales
+%
+% OUTPUTS: transformSignal_array - 2-D array containing wavelet transformed signal at all scales
+%          H1, H2 - figure handles to the various plots generated
+%
+% Based on 2-D wavelet codes from Geoff Spedding and wavelet codes from Christopher Torrence and Gilbert P. Compo, University of Colorado.
+% Acknowledgement:''Wavelet software was provided by C. Torrence and G. Compo, and is available at URL: http://atoc.colorado.edu/research/wavelets/''
+% 
 
-%================= Generate test signal====================================
-%============= Comment out appropriately to generate req signal============
+%==================== Debugging code   ====================================
+%==== Generate test signal - Comment out to generate req signal============
 
 % Fs = 8000; %Sampling rate, Hz
 % dt = 1/Fs;
 % t = 0:dt:1-dt;
 % f1 = 100;
 % f2 = 34; %test frequencies, in Hz
-% 
+%
 % test_signal1 = sin(2*pi()*f1*t);
 % test_signal2 = 0.*t;
-% 
+%
 % test_signal1 = sin(2*pi()*f1*t).*(t<=0.3)+sin(2*pi()*f2*t).*(t>0.3)+4*rand(1,length(t)).*(t>0.7);
 % test_signal2 = 0.*t;
 
 % test_signal1 = (sin(2*pi()*f1*t).*(t<=0.3)+sin(2*pi()*f2*t).*(t>0.3))+rand(1,length(t));
 % test_signal2 = (sin(2*pi()*(f1+10)*t).*(t<=0.3)+sin(2*pi()*f2*t).*(t>0.3));
-% 
+%
 % test_signal1 = sin(2*pi()*f1*t).*(t<=0.3)+sin(2*pi()*f2*t).*(t>0.3);
 % test_signal2 = (sin((2*pi()*f1*t)+(pi/2)).*(t<=0.3))+(sin(2*pi()*f2*t).*(t>0.3));
 
 % test_signal1 = sin(2*pi()*f1*t);
 % test_signal2 = sin((2*pi()*f2*t));
-% 
+%
 
 % test_signal = sin(2*pi()*4*t).*(t<0)+sin(2*pi()*1*t).*(t>=0);
 %  test_signal = test_signal1 + test_signal2;
@@ -38,39 +54,31 @@ function [transformSignal_array] = wavelet_run_script(test_signal,x_var,min_scal
 % clear t;
 %  t = linspace(0,1,length(test_signal));
 %  dt = t(2)-t(1);
-%==========================================================================
 % ====== calculate params for real signal =================================
 % test_signal = (rho5_fit_filt300 - rho5_sorted)';
 % R = 4; %radius of grid in cm
 % t = z_norm.*R; % height in cm
-
 % min_scale = 0.05; % specify in cm
 % max_scale = 1.5; %specify in cm
+%======================= End debugging code ===============================
 
-t = x_var;
 test_signal = test_signal';
-
-dt = abs(t(2) - t(1)); % height increment 
+dt = abs(x_var(2) - x_var(1)); % height increment
 % ds = .005; %scale increment - make sure it is greater than dt
-linlog_flag = 0; % 0 - linear scale; 1 - log scale
+linlog_flag = 1; % 0 - linear scale; 1 - log scale
 % =========================================================================
 
+n1 = length(test_signal); %length of original signal
+test_signal = [test_signal, zeros(1,(2^nextpow2(n1))-n1)]; % Pad with zeros to make length a power of 2 to speed up FFT
 test_signal_fft = fft(test_signal);
 n = length(test_signal_fft);
-signal_energy = sum(test_signal.^2)/n;%calculate energy in test signal
+signal_energy = sum(test_signal.^2)/n; %calculate energy in test signal
 e_fft = sum(abs(test_signal_fft).^2)/n^2;
 % =========================================================================
 
-
-
 [waveArray]=create_wave_array(n,dt);
 
-
-
-a0 = min_scale;
-a1 = max_scale;
-
-[scale,delta,da] = waveletscale(n,dt,ds,a0,a1,linlog_flag);
+[scale,delta,~] = waveletscale(n,dt,ds,min_scale,max_scale,linlog_flag);
 %  scale = 2*pi/n:.001:pi;
 transformSignal_array = zeros(length(scale),n);
 cg = 0;
@@ -116,13 +124,16 @@ elseif strcmp(wavelet_name,'morlet')
         emod = emod + enorm;
         e_wv(i) = sum(enorm);
     end
-%         morlet_fourier_factor = 4*pi()/(w0+sqrt(2+w0^2));
-%         freqScale = 1./(morlet_fourier_factor.*scale);
-%         pseudo_freq = freqScale;
-    pseudo_freq = w0./scale;%scale to pseudo-freq. w0 is center freq.
+    morlet_fourier_factor = 4*pi()/(w0+sqrt(2+w0^2));
+    Morlet_scale = morlet_fourier_factor.*scale;
+    freqScale = 1./(Morlet_scale);
+    %     pseudo_freq = w0./scale;%scale to pseudo-freq. w0 is center freq.
 else
     disp('Exciting new wavelets coming Fall 2015!')
 end
+
+transformSignal_array = transformSignal_array(:,1:n1);  % get rid of zero padding
+test_signal = test_signal(1:n1);
 
 %=============Normalize energy quantities by cg and print to screen========
 e_wv = e_wv./cg;%Energy at each scale
@@ -135,8 +146,8 @@ disp('Energy in original signal')
 signal_energy
 disp('Energy from PSD of original signal')
 e_fft
-figure(5)
-plot(scale,e_wv,'+r')
+H1 = figure(5);
+plot(Morlet_scale,e_wv,'+r')
 xlabel('Scale')
 ylabel('Energy')
 %==========================================================================
@@ -147,11 +158,9 @@ ylabel('Energy')
 % ylabel('Scale, cm');
 
 
-figure(9)
-xScale = t;
-
+H2 = figure(9);
 subplot(3,1,1)
-plot((xScale),test_signal)
+plot((x_var),test_signal)
 ylabel('\rho '', gm/cm^3')
 axis tight
 
@@ -159,21 +168,23 @@ subplot(3,1,2)
 % contourf(xScale,pseudo_freq,angle(transformSignal_array).*(abs(transformSignal_array)>0.005))
 % contourf(xScale,pseudo_freq,abs(transformSignal_array))
 % contourf(fliplr(xScale),scale./w0,abs(transformSignal_array))
-contourf((xScale),scale,abs(transformSignal_array))
+contourf((x_var),Morlet_scale,abs(transformSignal_array).*(abs(transformSignal_array) > 0.2*abs(max(max(transformSignal_array)))))
 ylabel('Scale, cm');
 
 subplot(3,1,3)
 % contourf(fliplr(xScale),scale./w0,angle(transformSignal_array).*(abs(transformSignal_array) > 1.5e-6))
-contourf((xScale),scale,angle(transformSignal_array).*(abs(transformSignal_array) > 9e-6))
+contourf((x_var),Morlet_scale,angle(transformSignal_array).*(abs(transformSignal_array) > 0.2*abs(max(max(transformSignal_array)))))
 
 ylabel('Scale, cm');
 xlabel('Z, cm')
 % contourf(xScale,pseudo_freq,abs(transformSignal_array))
 % contourf(xScale,pseudo_freq,abs(transformSignal_array).*(abs(transformSignal_array)>0.3))
-% 
-% figure(15)
-% [reconstructedSignal] = reconstructSignal(transformSignal_array,scale,waveArray,w0);
-% plot(reconstructedSignal,'b-')
-% hold on
-% plot(test_signal,'r--')
-% hold off
+%
+figure(15)
+[reconstructedSignal] = reconstructSignal(transformSignal_array,scale,waveArray,w0);
+plot(x_var,reconstructedSignal,'b-','DisplayName','Reconstructed Signal')
+hold on
+plot(x_var,test_signal,'r--','DisplayName','Original signal')
+hold off
+
+end
